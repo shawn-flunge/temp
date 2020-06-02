0f3a9c2615db98bf8873f66c0e0ada75a8c2d751
shawn <echun1234@gmail.com>
2020-06-02 16:58:44 +0900

    app_code

---
+++ temp/App_Code/Member.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+/// <summary>
+/// Member의 요약 설명입니다.
+/// </summary>
+public class Member
+{
+    string _conString;
+    public Member()
+    {
+        _conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+    }
+
+    public DataSet SelectMember()
+    {
+        SqlConnection con = new SqlConnection(_conString);
+
+        string sql = "select userid, password, name, phone from member";
+        SqlCommand cmd = new SqlCommand(sql,con);
+
+        SqlDataAdapter ad = new SqlDataAdapter();
+        ad.SelectCommand = cmd;
+
+        DataSet ds = new DataSet();
+        ad.Fill(ds);
+
+        return ds;
+    }
+
+    public DataSet SelectMemberByUserID(string userID)
+    {
+        SqlConnection con = new SqlConnection(_conString);
+
+        string sql = "select userid, password, name, phone from member";
+        SqlCommand cmd = new SqlCommand(sql, con);
+
+        SqlDataAdapter ad = new SqlDataAdapter();
+        ad.SelectCommand = cmd;
+
+        DataSet ds = new DataSet();
+        ad.Fill(ds);
+
+        return ds;
+    }
+    public int InsertMember(string userID, string password, string name, string phone)
+    {
+
+        return 0;
+    }
+    public int Update(string userID, string password, string name, string phone)
+    {
+        return 0;
+    }
+    public int DeleteMember(string userID)
+    {
+        return 0;
+    }
+
+}
---
+++ temp/App_Code/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// user의 요약 설명입니다.
+/// </summary>
+public class user
+{
+    string _conString;
+    public user()
+    {
+        _conString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+    }
+
+    public DataSet SelectUser()
+    {
+        SqlConnection con = new SqlConnection(_conString);
+
+        string sql = "select userid, password, name, phone from member";
+        SqlCommand cmd = new SqlCommand(sql, con);
+
+        SqlDataAdapter ad = new SqlDataAdapter();
+        ad.SelectCommand = cmd;
+
+        DataSet ds = new DataSet();
+        ad.Fill(ds);
+
+        return ds;
+    }
+
+    public int InsertUser(string userID, string password, string email)
+    {
+        SqlConnection con = new SqlConnection(_conString);
+
+        string sql = "insert into user values(@userid, @password, @email)";
+        SqlCommand cmd = new SqlCommand(sql, con);
+
+        cmd.Parameters.AddWithValue("@userid", userID);
+        cmd.Parameters.AddWithValue("@password", password);
+        cmd.Parameters.AddWithValue("@name", email);
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+
+        con.Close();
+
+        return 1;
+    }
+    public int UpdateUser(string userID, string password, string name, string phone)
+    {
+        return 0;
+    }
+    public int DeleteUser(string userID)
+    {
+        return 0;
+    }
+
+
+}
